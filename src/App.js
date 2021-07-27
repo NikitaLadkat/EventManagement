022fcb47c83fd3cf1655e1e01fca850a55ba3f0b
NikitaLadkat <63120813+NikitaLadkat@users.noreply.github.com>
2021-07-27 12:26:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginComponent from './LoginComponent'
+function App() {  
+  return (
+    <div> 
+      <h1>FindMyMovies</h1>
+      <LoginComponent />
+    </div>    
+  );
+}
+
+export default App;
